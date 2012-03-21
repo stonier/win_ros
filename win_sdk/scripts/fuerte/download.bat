007b21881a9ceda3c7bf9ac0859848e76a16c46f
Daniel Stonier <d.stonier@gmail.com>
2012-03-21 16:23:02 +0900

    win_sdk : fix rosinstall url bug.

--- win_sdk/scripts/fuerte/download.bat
+++ win_sdk/scripts/fuerte/download.bat
@@ -2,7 +2,7 @@ echo OFF
 
 REM rd /S /Q src
 REM call win-rosinstall --catkin src msvc_catkin.rosinstall
-call win-rosinstall --catkin src https://raw.github.com/stonier/win_ros/master/msvc_fuerte.rosinstall/msvc_catkin.rosinstall
+call win-rosinstall --catkin src https://raw.github.com/stonier/win_ros/master/msvc_fuerte.rosinstall
 call src\setup.bat
 cd src\win_ros\win_patches
 apply_msvc_patches
