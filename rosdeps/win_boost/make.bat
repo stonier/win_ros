d74459a55af62a42c658971107f68dbc2b5036cb
Daniel Stonier <d.stonier@gmail.com>
2012-03-21 14:59:59 +0900

    win_sdk: new fuerte build scripts.

--- rosdeps/win_boost/make.bat
+++ rosdeps/win_boost/make.bat
@@ -4,7 +4,7 @@ mkdir build
 cd build
 
 set WITHOUT_FLAGS=-DWITH_BZIP2=OFF -DWITH_DOXYGEN=OFF -DWITH_EXPAT=OFF -DWITH_ICU=OFF -DWITH_MPI=OFF -DWITH_VALGRIND=OFF -DWITH_XSLTPROC=OFF -DWITH_ZLIB=OFF
-set WITH_FLAGS=-DWITH_PYTHON=ON
+set WITH_FLAGS=-DWITH_PYTHON=ON -DCMAKE_INSTALL_PREFIX=C:\opt\rosdeps\x86
 set NO_STATIC=-DENABLE_STATIC:BOOL=FALSE -DENABLE_STATIC_RUNTIME:BOOL=FALSE
 set BUILD_PROJECTS=^
 preprocessor;concept_check;property_map;config;mpl;type_traits;function;iterator;smart_ptr;exception;^
@@ -19,6 +19,9 @@ ratio;rational;signals;signals2;statechart;tr1;units;uuid;wave
 SET FAILED_TO_BUILD_PROJECTS=chrono;
 set WILL_BE_DISABLED_PROJECTS_BECAUSE_OF_DEPS=regex;mpi;graph_parallel
 
-cmake %WITHOUT_FLAGS% %WITH_FLAGS% -DBUILD_PROJECTS:STRING=%BUILD_PROJECTS% %NO_STATIC% ..\boost
+cmake -G "NMake Makefiles" %WITHOUT_FLAGS% %WITH_FLAGS% -DBUILD_PROJECTS:STRING=%BUILD_PROJECTS% %NO_STATIC% ..\boost
+nmake
+REM nmake package
+REM nmake install
 cd ..
 
