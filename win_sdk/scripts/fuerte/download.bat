87448f26bbab8f0d08fd4e28e504318de55882eb
Daniel Stonier <d.stonier@gmail.com>
2012-03-25 15:28:40 +0900

    win_sdk : don't copy on itself.

--- win_sdk/scripts/fuerte/download.bat
+++ win_sdk/scripts/fuerte/download.bat
@@ -4,8 +4,8 @@ REM rd /S /Q src
 call win-rosinstall --catkin src https://raw.github.com/stonier/win_ros/master/msvc_fuerte.rosinstall
 call src\setup.bat
 cd src\win_ros\win_patches
-apply_msvc_patches
+call apply_msvc_patches
 cd ..\win_sdk\scripts\fuerte
-copy *.bat C:\work
+copy configure.bat C:\work
 copy *.cmake C:\work
 cd ../../../../..
