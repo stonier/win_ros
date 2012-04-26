0d09c550e77e32d62796f52801da46103e77cc5d
Daniel Stonier <d.stonier@gmail.com>
2012-04-26 12:41:34 +0900

    win_patches : msvc now apply diff style patches on stacks.

--- win_patches/apply_msvc_patches.bat
+++ win_patches/apply_msvc_patches.bat
@@ -1,2 +1,15 @@
-xcopy %cd%\updates\* %ROS_ROOT%\..\ /E /Y /EXCLUDE:exclude_svn.txt
 
+@REM We are assuming that all source stacks are two directories below this and nowhere else.
+@set DIR=%~dp0
+@set PATCH_DIR=%DIR%patches
+@set ROOT_DIR=%DIR%..\..
+
+@echo "Dir..........%DIR%"
+@echo "Patch Dir....%PATCH_DIR%"
+@echo "Source Dir...%ROOT_DIR%"
+
+cd %ROOT_DIR%
+@for /f %%a in ('dir /b %PATCH_DIR%\*.patch') do patch -p0 < %PATCH_DIR%\%%a
+
+@REM xcopy %cd%\updates\* %ROS_ROOT%\..\ /E /Y /EXCLUDE:exclude_svn.txt
+@cd %DIR%
