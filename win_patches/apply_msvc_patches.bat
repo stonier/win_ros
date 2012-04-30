2f46f79304ededca7393b83961525480c69e5752
Daniel Stonier <d.stonier@gmail.com>
2012-04-30 15:56:48 +0900

    win_patches : pretty print for apply_msvc_patches.

--- win_patches/apply_msvc_patches.bat
+++ win_patches/apply_msvc_patches.bat
@@ -4,9 +4,12 @@
 @set PATCH_DIR=%DIR%patches
 @set ROOT_DIR=%DIR%..\..
 
-@echo "Dir..........%DIR%"
-@echo "Patch Dir....%PATCH_DIR%"
-@echo "Source Dir...%ROOT_DIR%"
+@echo "PATCHING"
+@echo ""
+@echo "  Dir..........%DIR%"
+@echo "  Patch Dir....%PATCH_DIR%"
+@echo "  Source Dir...%ROOT_DIR%"
+@echo ""
 
 cd %ROOT_DIR%
 @for /f %%a in ('dir /b %PATCH_DIR%\*.patch') do patch -p0 < %PATCH_DIR%\%%a
