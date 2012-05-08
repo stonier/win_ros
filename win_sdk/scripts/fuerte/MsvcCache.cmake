0d30bd6abe71fb83181e7c30b76cad0d863ce20e
Daniel Stonier <d.stonier@gmail.com>
2012-05-08 18:48:56 +0900

    win_sdk : install root -> C:\opt\ros\fuerte and fix catkin build stacks variable.

--- win_sdk/scripts/fuerte/MsvcCache.cmake
+++ win_sdk/scripts/fuerte/MsvcCache.cmake
@@ -4,7 +4,7 @@ get_filename_component(CWD ${CMAKE_CURRENT_LIST_FILE} PATH)
 # WinRos
 ###########################
 set(ROSDEPS_ROOT "C:/opt/rosdeps/x86" CACHE STRING "System root for many rosdep installations.")
-set(INSTALL_ROOT "${CWD}/ros-sdk" CACHE PATH "Install root.")
+set(INSTALL_ROOT "C:/opt/ros/fuerte" CACHE PATH "Install root.")
 
 ###########################
 # CMake
@@ -18,9 +18,8 @@ set(CATKIN_ROSDEPS_PATH ${ROSDEPS_ROOT} CACHE STRING "Prefix to the rosdep root 
 ###########################
 # Catkin
 ###########################
+set(CATKIN_BUILD_STACKS "ALL" CACHE STRING "Semi-colon list of stacks to build.")
 set(CATKIN_BLACKLIST_STACKS "None" CACHE STRING "Semi-colon separated list of stacks to exclude from the build.")
-set(CATKIN_BUILD_PROJECTS "All" CACHE STRING "Semi-colon list of stacks to build.")
-#set(CATKIN_BUILD_PROJECTS "catkin;roscpp_core" CACHE STRING "Semi-colon list of stacks to build.")
 
 ###########################
 # Boost
