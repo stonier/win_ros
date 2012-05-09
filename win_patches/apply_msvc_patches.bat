9d65bebdd53c49ef50f9c210d33bb1456a2cbc5e
Daniel Stonier <d.stonier@gmail.com>
2012-05-09 12:35:49 +0900

    win_patches fixed rospack patch which was using absolute paths.

--- win_patches/apply_msvc_patches.bat
+++ win_patches/apply_msvc_patches.bat
@@ -1,18 +1,17 @@
+echo off
+REM We are assuming that all source stacks are two directories below this and nowhere else.
+set DIR=%~dp0
+set PATCH_DIR=%DIR%patches
+set ROOT_DIR=%DIR%..\..
 
-@REM We are assuming that all source stacks are two directories below this and nowhere else.
-@set DIR=%~dp0
-@set PATCH_DIR=%DIR%patches
-@set ROOT_DIR=%DIR%..\..
-
-@echo "PATCHING"
-@echo ""
-@echo "  Dir..........%DIR%"
-@echo "  Patch Dir....%PATCH_DIR%"
-@echo "  Source Dir...%ROOT_DIR%"
-@echo ""
+echo "PATCHING"
+echo ""
+echo "  Dir..........%DIR%"
+echo "  Patch Dir....%PATCH_DIR%"
+echo "  Source Dir...%ROOT_DIR%"
+echo ""
 
 cd %ROOT_DIR%
-@for /f %%a in ('dir /b %PATCH_DIR%\*.patch') do patch -p0 < %PATCH_DIR%\%%a
-
-@REM xcopy %cd%\updates\* %ROS_ROOT%\..\ /E /Y /EXCLUDE:exclude_svn.txt
-@cd %DIR%
+for /f %%a in ('dir /b %PATCH_DIR%\*.patch') do patch -p0 < %PATCH_DIR%\%%a
+REM patch -p0 < %PATCH_DIR%\rospack_windows.patch
+cd %DIR%
