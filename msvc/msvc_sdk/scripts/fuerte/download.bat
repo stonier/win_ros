3bf2ce0e723b3dd7b5f0775e0133bebb8440bf2e
Daniel Stonier <d.stonier@gmail.com>
2012-06-19 16:36:58 +0900

    no longer need the --catkin, win-rosinstall 0.6.17+ handles it.

--- msvc/msvc_sdk/scripts/fuerte/download.bat
+++ msvc/msvc_sdk/scripts/fuerte/download.bat
@@ -1,7 +1,7 @@
 @echo off
 REM rd /S /Q src
 set PWD=%~dp0
-call win-rosinstall --catkin src https://raw.github.com/stonier/win_ros/master/msvc_fuerte.rosinstall
+call win-rosinstall src https://raw.github.com/stonier/win_ros/master/msvc_fuerte.rosinstall
 call src\setup.bat
 cd src\win_ros\win_patches
 call apply_msvc_patches
