b22990f1d5f83890adf284bd27b1f80c3a72d441
Daniel Stonier <d.stonier@gmail.com>
2012-06-26 17:49:20 +0900

    win_rosinstall: added script to upload - more convenient to build now.

--- win_rosinstall/make.bat
+++ win_rosinstall/make.bat
@@ -1,18 +1,22 @@
-ECHO OFF
-REM
-REM Make sure you have bumped the version in setup.py and scripts/winrosinstall.py before doing this.
-REM
+@ECHO OFF
 
 IF X%1==Xclean GOTO Clean
-IF X%1==Xdistro GOTO Distro
+IF X%1==Xdistro GOTO Compile
 IF X%1==Xall GOTO Compile
 IF X%1==Xcompile GOTO Compile
+IF X%1==Xupload GOTO Compile
+IF X%1==Xversion GOTO Version
 
-ECHO "Invalid usage: call with args from ['clean', 'all', 'distro']"
+ECHO "Invalid usage: call with args from ['clean', 'all', 'distro', 'upload', 'version']"
+GOTO End
+
+: Version
+echo.
+echo "Make sure you bump the version in setup.py if necessary."
 GOTO End
 
 :Compile
-IF NOT EXIST %cd%\src\vcstools (
+IF NOT EXIST %cd%\scripts\win-rosinstall.py (
   REM This is tip 02/03/2012
   hg clone -r 15d0b6e38e2e https://kforge.ros.org/vcstools/hg vcstools
   hg clone -r 4ab7a92fdf07 https://kforge.ros.org/vcstools/rosinstall rosinstall
@@ -23,8 +27,31 @@ IF NOT EXIST %cd%\src\vcstools (
   rd /S /Q rosinstall
   REM copy /Y %cd%\patches\svn.py %cd%\src\vcstools
   REM copy /Y %cd%\patches\multiproject_cmd.py %cd%\src\rosinstall
+) ELSE (
+  echo.
+  echo "Already prepped"
 )
+IF X%1==Xdistro GOTO Distro
 IF X%1==Xall GOTO Distro
+IF X%1==Xupload GOTO Distro
+GOTO End
+
+:Distro
+REM python setup.py bdist_wininst
+IF NOT EXIST %cd%\dist (
+  python setup.py bdist_msi
+) ELSE (
+  echo.
+  echo "Msi installer already built"
+)
+IF X%1==Xupload GOTO Upload
+IF X%1==Xall GOTO Upload
+
+:Upload
+cd dist
+scp *.msi files@files.yujinrobot.com:pub/appupdater/python/2.7/
+GOTO End
+
 
 :Clean
 rd /S /Q %cd%\build
@@ -34,10 +61,7 @@ rd /S /Q %cd%\src\rosinstall
 rm %cd%\scripts\win-rosinstall.py
 GOTO End
 
-REM python setup.py bdist_wininst
 
-:Distro
-python setup.py bdist_msi
 GOTO End
 
 :End
