ada0e83a2f528f88c73d6973c4650eb197f32048
Daniel Stonier <d.stonier@gmail.com>
2012-06-27 14:20:15 +0900

    msvc_sdk : unstable option for download.

--- msvc/msvc_sdk/scripts/fuerte/download.bat
+++ msvc/msvc_sdk/scripts/fuerte/download.bat
@@ -1,7 +1,14 @@
 @echo off
-REM rd /S /Q src
+
+rem rd /S /Q src
+
 set PWD=%~dp0
-call win-rosinstall src https://raw.github.com/stonier/win_ros/master/msvc_fuerte.rosinstall
+
+if X%1==Xunstable (
+  call win-rosinstall src https://raw.github.com/stonier/win_ros/master/msvc_unstable.rosinstall
+) else (
+  call win-rosinstall src https://raw.github.com/stonier/win_ros/master/msvc_fuerte.rosinstall
+)
 call src\setup.bat
 cd src\win_ros\win_patches
 call apply_msvc_patches
