9319309647b60fbb516efdae46468ff68681a5c1
Daniel Stonier <d.stonier@gmail.com>
2012-06-27 12:43:31 +0900

    win_rospkg: added build script that also adds rosversion.bat script.

---
+++ rosdeps/win_rospkg/make.bat
@@ -0,0 +1,62 @@
+@ECHO OFF
+
+set COMMAND=%1
+if X%COMMAND%==X set COMMAND=all
+if X%COMMAND%==Xhelp goto Help
+if X%COMMAND%==Xclean goto Clean
+if X%COMMAND%==Xall goto Download
+if X%COMMAND%==Xdownload goto Download
+if X%COMMAND%==Xdistro goto Distro
+if X%COMMAND%==Xupload goto Upload
+goto Help
+
+:Help
+echo "Usage: call with args from ['clean', 'all', 'download', 'distro']"
+goto End
+
+:Download
+IF NOT EXIST %cd%\rospkg (
+  echo.
+  echo "Downloading and patching code."
+  call git clone https://github.com/ros/rospkg.git
+  copy scripts\rosversion.bat rospkg\scripts\rosversion.bat
+  rem Add rosversion.bat to the list of scripts to install
+  sed -e s:rosversion"'":rosversion"'","'"scripts/rosversion.bat"'":g rospkg/setup.py > setup.py.tmp
+  copy /Y setup.py.tmp rospkg\setup.py
+  rm setup.py.tmp
+) ELSE (
+  echo.
+  echo "Already cloned the rospkg repository."
+)
+if X%COMMAND%==Xall (
+  goto Distro
+) else (
+  goto End
+)
+
+:Distro
+echo "Building msi installer."
+cd rospkg
+python setup.py bdist_msi
+cd ..
+if X%COMMAND%==Xall (
+  goto Upload
+) else (
+  goto End
+)
+
+:Upload
+echo "Uploading to file server."
+cd rospkg/dist
+scp *.msi files@files.yujinrobot.com:pub/appupdater/python/2.7/
+cd ..\..
+goto End
+
+:Clean
+rd /S /Q %cd%\rospkg
+GOTO End
+
+
+GOTO End
+
+:End
---
+++ rosdeps/win_rospkg/scripts/rosversion.bat
@@ -0,0 +1,7 @@
+@echo off
+
+rem Used to help execute rosversion because windows is trivially fixated 
+rem on extensions.
+
+set DIR=%~dp0
+python %DIR%\rosversion %*
