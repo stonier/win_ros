2558fcce2080420f07331404f6a5356033eed1de
Daniel Stonier <d.stonier@gmail.com>
2012-06-27 16:50:39 +0900

    msvc_sdk : cleaned solution.

---
+++ msvc/msvc_sdk/sdk_projects/fuerte/sdk-tutorials/clean.bat
@@ -0,0 +1,16 @@
+
+@echo off
+
+rm -f ros.sdf
+rm -f ros.suo
+rm -rf Debug
+rm -rf ipch
+rm -rf init_test
+rm -rf Release
+rm -rf listener/Debug
+rm -rf listener/Release
+rm -rf talker/Debug
+rm -rf talker/Release
+rm -rf rostime/Debug
+rm -rf rostime/Release
+
