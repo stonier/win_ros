6cd552e6e7234ede78af90d190b543f1a72dd257
Daniel Stonier <d.stonier@gmail.com>
2012-06-28 09:08:07 +0900

    Add commented option for experimentally using the visual studio generator in configure.bat. This references #53.

--- msvc/msvc_sdk/scripts/fuerte/configure.bat
+++ msvc/msvc_sdk/scripts/fuerte/configure.bat
@@ -13,8 +13,19 @@
 
 @REM ************* Cmake *************
 
-@REM To change build mode, configure CMAKE_BUILD_TYPE in MsvcCache.cmake or add -DCMAKE_BUILD_TYPE=... here.
+@rem Some comments:
+@rem 
+@rem   To change build mode, configure CMAKE_BUILD_TYPE in MsvcCache.cmake or add -DCMAKE_BUILD_TYPE=... here.
+@rem
+@rem   When choosing your generator, make sure you call the appropriate vcsvarall.bat script
+@rem   before running this script.
+
+@rem To use the windows sdk nmake generator (default)
 cmake -G "NMake Makefiles" -C "%WORKSPACE%\MsvcCache.cmake" -DCMAKE_USER_MAKE_RULES_OVERRIDE:STRING="%WORKSPACE%\MsvcFlags.cmake" %SRC_DIR%
+
+@rem To use the visual studio generator, uncomment the following (experimental)
+@rem cmake -G "Visual Studio 10" -C "%WORKSPACE%\MsvcCache.cmake" -DCMAKE_USER_MAKE_RULES_OVERRIDE:STRING="%WORKSPACE%\MsvcFlags.cmake" %SRC_DIR%
+
 @cd ..
 @GOTO End
 
