44c1e4281af3f39a11da2c0d22509278124fc15a
Daniel Stonier <d.stonier@gmail.com>
2012-07-17 15:55:14 +0900

    win-rosinstall -> rosinstall.

--- msvc/msvc_sdk/scripts/fuerte/download.bat
+++ msvc/msvc_sdk/scripts/fuerte/download.bat
@@ -5,9 +5,9 @@ rem rd /S /Q src
 set PWD=%~dp0
 
 if X%1==Xunstable (
-  call win-rosinstall src https://raw.github.com/stonier/win_ros/master/msvc_unstable.rosinstall
+  call rosinstall src https://raw.github.com/stonier/win_ros/master/msvc_unstable.rosinstall
 ) else (
-  call win-rosinstall src https://raw.github.com/stonier/win_ros/master/msvc_fuerte.rosinstall
+  call rosinstall src https://raw.github.com/stonier/win_ros/master/msvc_fuerte.rosinstall
 )
 call src\setup.bat
 cd src\win_ros\win_patches
